93d64566f90468a228c671f2cd7772924005b724
dlfgeo <azdforsyth@gmail.com>
2024-03-05 20:24:00 -0800

    Changed the title of the chat bot, added an option button and date selectors to pre-populate the prompt.

--- Chatbot.py
+++ Chatbot.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain.llms import OpenAI
 
-st.title('🦜🔗 Quickstart App')
+st.title(f'\u2708 Travel App')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
@@ -9,8 +9,15 @@ def generate_response(input_text):
     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
     st.info(llm(input_text))
 
+#Add a radio button option to provide context for the location. This will also help to route the prompt to the appropriate RAG path
+#Add a date option as well to preset the prompt.
+with st.sidebar:
+   city =  st.radio("Where Are You travelling?:sunglasses:", ['London', 'NYC'])
+   d1 = st.date_input("Travel Start Date", value="today")
+   d2 = st.date_input("Travel End Date", value="today")
+
 with st.form('my_form'):
-    text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+    text = st.text_area('Enter text:', f'I\'m travelling to {city} from {d1} to {d2} and I would like to know more about')
     submitted = st.form_submit_button('Submit')
     if not openai_api_key.startswith('sk-'):
         st.warning('Please enter your OpenAI API key!', icon='⚠')
