00f7439909ea6c1ac3fa1d582ff471544b099fb3
Iqbal Mohomed <iqbaliia@gmail.com>
2024-03-16 17:02:53 -0400

    Add food retrieval to UI

--- Chatbot.py
+++ Chatbot.py
@@ -5,12 +5,19 @@ from langchain_openai import OpenAI
 
 st.title(f'\u2708 Travel App')
 
+#load fooddb from file
+import pickle
+f = open('food_db.pkl', 'rb')
+food_db = pickle.load(f)
+
 openai_api_key = st.text_input('OpenAI API Key', type='password')
 
 #Basic LLM Call
 def generate_response(input_text):
     llm = OpenAI(temperature=0.1, openai_api_key=openai_api_key,max_tokens=-1)
-    st.info(llm.invoke(input_text))
+    generated_text = llm.invoke(input_text)
+    st.session_state['generated_text'] = generated_text
+
 
 with st.form('my_form'):
     city_to_visit = st.selectbox(
@@ -28,7 +35,23 @@ with st.form('my_form'):
     text = ""
     if not openai_api_key.startswith('sk-'):
         st.warning('Please enter your OpenAI API key!', icon='⚠')
-    if submitted and  city_to_visit!= None and openai_api_key.startswith('sk-'):
+    if submitted and  city_to_visit!= None and openai_api_key.startswith('sk-') and not 'generated_text' in st.session_state:
         prompt = f"""You are a helpful assistant that provides travel suggestions for a desired city based on 
         a user's interests. Provide a list of up to 10 attractions in the {city_to_visit} for someone interested in {travel_interests}. For each item in the list, provide a short description about the attraction."""
         generate_response(prompt)
+    if 'generated_text' in st.session_state:
+        generated_text = st.session_state['generated_text']
+        st.info(generated_text)
+
+st.divider()
+
+favfood = st.radio('What kind of restaurants are you interested in?',
+    ('Pizza', 'Burgers', 'Fast Food', 'Upscale Restaurant'), index=None,
+                   disabled = ('generated_text' not in st.session_state)
+    )
+if favfood == "Pizza" or favfood == "Burgers" or favfood == "Fast Food" or favfood == "Upscale Restaurant":
+    # find matching review in food_db given city_to_visit and favfood
+    for (city, food_type, doc) in food_db:
+        if city == city_to_visit and food_type == favfood:
+            st.write(doc)
+
